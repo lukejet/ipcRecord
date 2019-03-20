dbe207aa901079ed7b11bae19644d8c7b7a401c2
xuluzhu <xuluzhu@tucodec.com>
2019-03-20 13:32:53 +0800

    update install_rec.sh
    add chmod +x for the shell files.

--- install_rec.sh
+++ install_rec.sh
@@ -2,6 +2,12 @@
 
 mkdir -p /var/record/
 
+cp record.sh /usr/local/bin/
+cp mg_recs.sh /usr/local/bin/
+
+chmod + /usr/local/bin/record.sh
+chmod + /usr/local/bin/mg_recs.sh
+
 crontab -l > crontab.bak
 echo "*/5 * * * * /usr/local/bin/record.sh 5" >> crontab.bak
 echo "*/30 * * * * /usr/local/bin/mg_recs.sh" >> crontab.bak
