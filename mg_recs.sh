d64bd6615964cee69c708bdca95d0b812f48c4b0
lukejet <lukejet@163.com>
2020-04-14 23:18:39 +0800

    update mg_recs.sh, support archieve_video_files to directories.

--- mg_recs.sh
+++ mg_recs.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 IPC_NAME=$1
+REC_PATH=/var/record/$IPC_NAME
+
 # manage records by date
+
+function archieve_video_files
+{
+# move the old video files to the old day directory.
+
+CUR_SEC=`date +%s`
+YESTERDAY=`expr ${CUR_SEC} - 86400`
+YESTERDAY=`date -d "@${YESTERDAY}" +%y%m%d`
+YDAY_DIR=/var/record/$IPC_NAME/${IPC_NAME}_$YESTERDAY/
+mkdir -p $YDAY_DIR
+
+FILES=${REC_PATH}/${IPC_NAME}_${YESTERDAY}*
+
+mv $FILES $YDAY_DIR
+
+}
 
 function manage_date
 {
@@ -14,14 +32,13 @@ echo "manage the record files by date."
 #HOUR=`date +%H`
 
 CUR_SEC=`date +%s`
-OLD_SEC=`expr ${CUR_SEC} - 4000`
+# 864000secs is 10days
+OLD_SEC=`expr ${CUR_SEC} - 864000`
 OLD_TIME=`date -d "@${OLD_SEC}" +%T`
 
 echo $OLD_TIME
 
-REC_PATH=/var/record/$IPC_NAME/
-
-FILES=${REC_PATH}${IPC_NAME}_`date -d "@${OLD_SEC}" +%y%m%d_%H`_*.mkv
+FILES=${REC_PATH}/${IPC_NAME}_`date -d "@${OLD_SEC}" +%y%m%d_%H`_*
 
 echo "will remove old record files $FILES ..."
 rm -rf $FILES
@@ -37,4 +54,4 @@ function manage_space()
 
 MNG_TYP=$1
 manage_date 
-
+archieve_video_files
